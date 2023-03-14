2beeeb6c3177894ae09149d2125c94e19de75e27
Sergei Voronezhskii <sergei.v@tonlabs.io>
2023-03-14 13:46:08 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use serde_json::json;
+use std::{
+    env,
+    sync::Arc,
+};
+use ton_client::{
+    ClientConfig,
+    ClientContext,
+    net::{
+        ParamsOfQuery,
+        NetworkConfig,
+        query,
+    },
+};
+
+#[tokio::main]
+async fn main() {
+    let network = match env::var("TON_NETWORK_ADDRESS") {
+        Ok(val) => val,
+        Err(_) => "http://localhost/graphql".to_owned()
+    };
+
+    dbg!(network.clone());
+
+    let context = Arc::new(
+        ClientContext::new(ClientConfig {
+            network: NetworkConfig {
+                endpoints: Some(vec![network.to_owned()]),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .unwrap(),
+    );
+
+    let output = query(
+        context.clone(),
+        ParamsOfQuery {
+            query: r#"query{info{version}}"#.to_string(),
+            variables: json!({}),
+            ..Default::default()
+        }
+    )
+    .await
+    .unwrap();
+    dbg!(output);
+}
